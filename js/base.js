8eba53848d120360b0d9e42680af65c464649ebe
Sun Yan <sunyan0221@gmail.com>
2015-03-11 21:28:24 +0800

    速度调整

--- js/base.js
+++ js/base.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
     var slogan3=$("#slogan3");
     var slogan4=$("#slogan4");
     setTimeout(function(){
-        slogan1.fadeIn("5000")
+        slogan1.fadeIn("5000");
     },1000);
     setTimeout(function(){
-        slogan2.fadeIn("5000")
-    },2000);
-    setTimeout(function(){
-       slogan3.fadeIn("5000")
+        slogan2.fadeIn("5000");
     },3000);
     setTimeout(function(){
+       slogan3.fadeIn("5000")
+    },5000);
+    setTimeout(function(){
         slogan4.fadeIn("5000")
-    },4000);
+    },7000);
 });
